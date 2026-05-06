98e0f68a670976b0677d4f59156f6d203fb82e4a
Dong Ha Cong <donghc2@fpt.com>
2026-05-06 12:04:52 +0700

    clean prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,12 +7,4 @@ export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL!,
   },
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      const { default: pg } = await import('pg')
-      const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
-      return new PrismaPg(pool)
-    },
-  },
 })
